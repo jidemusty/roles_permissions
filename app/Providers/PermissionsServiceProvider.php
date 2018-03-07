df4c5daae0da288a16cf8fe72c4b161cce6bde22
jidemusty <babajidemusty@gmail.com>
2018-03-07 10:55:21 +0100

    custom blade directives

--- app/Providers/PermissionsServiceProvider.php
+++ app/Providers/PermissionsServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Gate;
 use App\Permission;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class PermissionsServiceProvider extends ServiceProvider
@@ -19,6 +20,14 @@ class PermissionsServiceProvider extends ServiceProvider
             Gate::define($permission->name, function ($user) use ($permission) {
                 return $user->hasPermissionTo($permission);
             });
+        });
+
+        Blade::directive('role', function ($role) {
+            return "<?php if(auth()->check() && auth()->user()->hasRole({$role})): ?>";
+        });
+
+        Blade::directive('endrole', function ($role) {
+            return "<?php endif; ?>";
         });
     }
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,6 +37,9 @@
                             <li><a class="nav-link {{ Request::is('login') ? 'active' : '' }}" href="{{ route('login') }}">Login</a></li>
                             <li><a class="nav-link {{ Request::is('register') ? 'active' : '' }}" href="{{ route('register') }}">Register</a></li>
                         @else
+                            @role('admin')
+                                <li><a class="nav-link" href="">Admin</a></li>
+                            @endrole
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
